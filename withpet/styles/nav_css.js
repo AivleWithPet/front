b6732b523b92f6348203ae5c5909f0e624f68b10
seojong789 <seojong789@naver.com>
2023-05-24 01:42:28 +0900

    #8 - navBar 추가
    error 페이지와 css가 겹침 - 해결해야한다.

---
+++ withpet/styles/nav_css.js
@@ -0,0 +1,26 @@
+import styled from '@emotion/styled';
+
+export const Nav = styled.div `
+  height: 70px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  /* max-width: 1000px; */
+  margin: auto;
+  width: 100%;
+
+  text-decoration: none;
+  padding-left: 20px;
+  padding-right: 20px;
+  color: black;
+
+  background-color: white;
+`
+
+export const NavA = styled.div `
+  text-decoration: none;
+  padding-left: 20px; 
+  padding-right: 20px;
+  color: black;
+`
+
