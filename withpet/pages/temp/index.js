bdd758a71db5019f88390d5288543b6b0d1e4fcd
chaewon <kchae10824@gmail.com>
2023-05-24 21:13:05 +0900

    #11 close

---
+++ withpet/pages/temp/index.js
@@ -0,0 +1,12 @@
+import React, { useRef, useState } from "react";
+
+const TempPage = () => {
+
+    return (
+        <div>
+            임시 확인용 입니다.
+
+        </div>);
+
+};
+export default TempPage;
