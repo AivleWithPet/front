667592a9dd0eef6cc6caf44e293c41c3a260921d
MUYEONKIM <rlaandus2@gmail.com>
2023-06-07 22:52:51 +0900

    intergration2

--- src/commons/layout/index.js
+++ src/commons/layout/index.js
@@ -1,9 +1,18 @@
 import NavBar from "../../components/navBar/navBar.js"
+import NavBar2 from "../../components/navBar2/navBar2.js"
+import {useRouter} from "next/router"
+
+const Hidden = [
+    "/"
+]
 
 export default function Layout(props) {
+    const router = useRouter()
+    const isHidden = Hidden.includes(router.asPath)
     return (
         <>
-            <NavBar />
+            {isHidden && <NavBar />}
+            {!isHidden && <NavBar2 />}
             <div>{props.children}</div>
         </>
         )
