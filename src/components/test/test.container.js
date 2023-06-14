2976763b8ca7477dc3f71dd7e59930e40649501f
MUYEONKIM <rlaandus2@gmail.com>
2023-06-14 20:55:51 +0900

    #close

--- src/components/test/test.container.js
+++ src/components/test/test.container.js
@@ -1,98 +1,100 @@
-import { Anchor } from 'antd';
-import { All, Div, Div1, Div2, Div3, Div4, Div5, Div6 } from '../../../styles/test.style';
+import { Anchor, Image  } from 'antd';
+import {ReconciliationOutlined} from '@ant-design/icons'
+import styled from '@emotion/styled';
+import { All, Div, Div1, Div11, Div1_1, Div1_2, Div2,
+   Div3, Div3_1, Div3_2, Div3_3, Div3_3_1, Div3_3_2, 
+   Div4, Div5, Div6 } from '../../../styles/test.style'; 
+import axios from 'axios'
+import { useState, useEffect } from 'react';
+
+const MyAnchor = styled(Anchor)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-decoration: none;
+  height: 70px;
+  background-color: #ffffff;
+  border: 1px gray;
+`;
+
+const { Link } = Anchor;
+
+const MyLink = styled(Link)`
+  font-size: 15px;
+  text-decoration: none;
+`
+
+const MyIcon = styled(ReconciliationOutlined)`
+  color: red;
+  font-size: 30px;
+`
 
 export default function TestContainer() {
+	  const [dogsUrl,setDogUrl] = useState("")  
+	  useEffect(()=>{
+	  	const fetchDog = async ()=>{
+	  		const result = await axios.get("https://dog.ceo/api/breeds/image/random")
+	  		console.log(result.data.message)
+	  		setDogUrl(result.data.message)
+	  			} 
 
-    const AnchorStyle = {
-        border: '1px solid',
-        height: '70vh',
-        margin: '80px 30px',
-        fontsize: '100px',
-    }
+	  fetchDog()
+	  	},[])
 
+      const [messagesUrl,setMessageUrl] = useState("")  
+      useEffect(()=>{
+        const fetchMesage = async ()=>{
+          const result = await axios.get("https://api.breakingbadquotes.xyz/v1/quotes")
+          console.log(result.data[0].quote)
+          setMessageUrl(result.data[0].quote)
+            } 
+  
+        fetchMesage()
+        },[])
+  
     return (
+      <>
+        <MyAnchor direction='horizontal' targetOffset='30px'>
+          <MyLink href="#part1" title="Page1" />
+          <MyLink href="#part2" title="Page2" />
+          <MyLink href="#part3" title="Page3" />
+        </MyAnchor>
         <All>
             <Div>
-              <Anchor
-                style={AnchorStyle}
-                affix={true}
-                items={[
-                  {
-                    key: 'part-1',
-                    href: '#part-1',
-                    title: 'asdfsadf',
-                  },
-                  {
-                    key: 'part-2',
-                    href: '#part-2',
-                    title: 'Part 2',
-                  },
-                  {
-                    key: 'part-3',
-                    href: '#part-3',
-                    title: 'Part 3',
-                  },
-                  {
-                    key: 'part-4',
-                    href: '#part-4',
-                    title: 'Part 4',
-                  },
-                  {
-                    key: 'part-5',
-                    href: '#part-5',
-                    title: 'Part 5',
-                  },
-                  {
-                    key: 'part-6',
-                    href: '#part-6',
-                    title: 'Part 6',
-                  },
-                ]}
-              />
-            </Div>
-            <div>
-                <Div1 id="part-1">
-                    <div>as</div>
-                    <div>asd</div>
+                <Div1 id="part1">
+                  <Div11>
+                    <Div1_1>
+                      <MyIcon />
+                      <div>진단결과입니다.</div>
+                    </Div1_1>
+                    <Div1_2>{messagesUrl}</Div1_2>
+                  </Div11>
                 </Div1>
-                <Div2  id="part-2">
-                    <div>as</div>
-                    <div>asd</div>
-                </Div2>
-                <Div3  id="part-3">
-                    <div>
-                        <div>
-                            asdf
-                        </div>
-                        <div>
-                            <div>sdaf</div>
-                            <div>sdf</div>
-                        </div>
-                    </div>
-                </Div3>
-                <Div4  id="part-4">
-                    <div>
-                        <div>
-                            asdf
-                        </div>
-                        <div>
-                            sadf
-                        </div>
-                    </div>
-                    <div>
-                        <div>
-                            asdf
-                        </div>
-                        <div>
-                            <div>asdf</div>
-                            <div>asdf</div>
-                            <div>asdf</div>
-                        </div>
-                    </div>
+                <Div4 id="part2">
+                    <Div5>
+                      <Div2>
+                        <Image 
+                          width={500}
+                          height={500}
+                          src = {dogsUrl}
+                        />
+                      </Div2>
+                        <Div3_3_2>{messagesUrl}</Div3_3_2>
+                    </Div5>
                 </Div4>
-                <Div5  id="part-5"></Div5>
-                <Div6  id="part-6"></Div6>
-            </div>
+                <Div3 id="part3">
+                    <Div3_1>
+                        <Div3_2>
+                        {messagesUrl}
+                        </Div3_2>
+                        <Div3_3>
+                            <Div3_3_1>{messagesUrl}</Div3_3_1>
+                            <Div3_3_2>{messagesUrl}</Div3_3_2>
+                        </Div3_3>
+                    </Div3_1>
+                </Div3>
+            </Div>
         </All>     
-    )       
-}       
+    </>)       
+}
--- styles/test.style.js
+++ styles/test.style.js
@@ -4,34 +4,54 @@ export const All =styled.div`
     display: flex;
     flex-direction: row;
     justify-content: space-between;
-    border: 3px solid green;
 `
 
 export const Div = styled.div`
-    width: 15vw;
-    padding: 20px;
-    border: 3px solid red;
-
+    padding-left: 7% ;
 `
 export const Div1 = styled.div`
-    width: 85vw;
     height: 100vh;
     text-Align: 'center';
     background: 'red';
-    border: 1px solid blue;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+`
+
+export const Div11 = styled.div`
+    width: 90%;
+    height: 80%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    padding: 5%;
+    border: 1px solid gray;
+    border-radius: 100px;
+    box-shadow: 1000px;
 `
 
 export const Div1_1 = styled.div`
+    border: 1px solid black;
+    height: 15%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+    padding: 5px 0;
+`
+
+export const Div1_2 = styled.div`
+    border: 1px solid black;
+    height: 65%;
 `
 
 export const Div2 = styled.div`
-    width: 85vw;
-    height: 100vh;
-    text-Align: 'center';
-    background: 'rgba(0,0,255,0.02)';
-`
-
-export const Div2_1 = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding-left: 5%;
+    border: 1px solid red;
 `
 
 export const Div3 = styled.div`
@@ -39,41 +59,70 @@ export const Div3 = styled.div`
     height: 100vh;
     text-Align: 'center';
     background: '#FFFBE9';
+    display: flex;
+    justify-content: center;
+    margin-top: 130px;
 `
 
 export const Div3_1 = styled.div`
+    border: 1px solid;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+    height: 80%;
+    width: 90%;
+    margin-top: 100px;
+    padding-bottom: 60px;
+    border: 1px solid blue;
+    border-radius: 100px;
 `
 
 export const Div3_2 = styled.div`
+    border: 1px solid;
+    margin-top: 50px;
+    width: 80%;
+    height: 10%;
+`
+
+export const Div3_3 = styled.div`
+    display:flex;
+    flex-direction: row;
+    border: 1px solid;
+    width: 80%;
+    height: 80%;
+    justify-content: space-between;
+`
+
+export const Div3_3_1 = styled.div`
+    height: 100%;
+    border: 1px solid;
+    width: 45%;
+`
+
+export const Div3_3_2 = styled.div`
+    height: 100%;
+    border: 1px solid;
+    width: 45%;
 `
 
 export const Div4 = styled.div`
-    width: 85vw;
+    margin-top: 130px;
+    width: 100%;
     height: 100vh;
-    text-Align: 'center';
-    background: '#F4EAD5';
+    display: flex;
+    justify-content: center;
+    align-items: center;
 `
 
-export const Div4_1 = styled.div`
-`
-
-export const Div4_2 = styled.div`
-`
-
-export const Div4_3 = styled.div`
-`
-
-export const Div4_4 = styled.div`
-`
 export const Div5 = styled.div`
-    width: 85vw;
-    height: 100vh;
-    text-Align: 'center';
-    background: '#DAE2B6';
-`
-export const Div6 = styled.div`
-    width: 85vw;
-    height: 100vh;
-    text-Align: 'center';
-    background: '#CCD6A6';
+    display:flex;
+    flex-direction: row;
+    border: 1px solid;
+    width: 90%;
+    height: 80%;
+    padding: 5%;
+    justify-content: space-between;
+    border: 1px solid blue;
+    border-radius: 100px;
 `
