6235d0d4a781c7048b7ac94a98b0d8485e478d42
Seo Jong-Pil <seojong789@naver.com>
2023-07-26 23:34:48 +0900

    FEAT: Redux나 Zustand로 고칠겁니다.

--- src/commons/layout/index.js
+++ src/commons/layout/index.js
@@ -1,19 +1,32 @@
 import NavBar from "../../components/navBar/navBar.js"
-import NavBar2 from "../../components/navBar2/navBar2.js"
 import {useRouter} from "next/router"
-
-const Hidden = [
-    "/"
-]
 
 export default function Layout(props) {
     const router = useRouter()
-    const isHidden = Hidden.includes(router.asPath)
     return (
         <>
-            {isHidden && <NavBar />}
-            {!isHidden && <NavBar2 />}
+            <NavBar />
             <div>{props.children}</div>
         </>
         )
 }
+
+// import NavBar from "../../components/navBar/navBar.js"
+// import NavBar2 from "../../components/navBar2/navBar2.js"
+// import {useRouter} from "next/router"
+
+// const Hidden = [
+//     "/"
+// ]
+
+// export default function Layout(props) {
+//     const router = useRouter()
+//     const isHidden = Hidden.includes(router.asPath)
+//     return (
+//         <>
+//             {isHidden && <NavBar />}
+//             {!isHidden && <NavBar2 />}
+//             <div>{props.children}</div>
+//         </>
+//         )
+// }
