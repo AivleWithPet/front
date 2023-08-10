f9a5a0e4e8d05fade3e53fe16ea8245a5c84e43d
chapssalddeock <kchae10824@gmail.com>
2023-08-10 22:54:39 +0900

    add: pet profile layout

--- src/components/mypage/sidebar.container.js
+++ src/components/mypage/sidebar.container.js
@@ -1,6 +1,17 @@
 import React, { useState, useEffect } from "react";
 import { Menu } from "antd";
 
+const Profile = ({ data }) => {
+  return (
+    <div>
+      <h2>{data.petName}</h2>
+      <p>종류: {data.species}</p>
+      <p>출생 연도: {data.birthYear}</p>
+      <p>추가 정보: {data.info}</p>
+    </div>
+  );
+};
+
 export default function SideBar({ petList, selectedItem, onItemClick }) {
   useEffect(() => {
     if (petList.length > 0 && !selectedItem) {
@@ -8,6 +19,9 @@ export default function SideBar({ petList, selectedItem, onItemClick }) {
       onItemClick(petList[0].petId);
     }
   }, [petList, selectedItem, onItemClick]);
+
+  // petList에서 selectedItem과 일치하는 항목 찾기..근데 이거 selectedItem에 따라 안 바뀜..이유가 뭘까?
+  const selectedPet = petList.find((item) => item.petId === selectedItem);
 
   return (
     <Menu
@@ -21,10 +35,32 @@ export default function SideBar({ petList, selectedItem, onItemClick }) {
     >
       {petList.length > 0 ? (
         petList.map((item) => (
-          <Menu.Item key={item.petId}>{item.petName}</Menu.Item>
+          <Menu.Item
+            key={item.petId}
+            style={{
+              textAlign: "right",
+              height: "8vh",
+              fontSize: "1.8vh",
+            }}
+          >
+            {item.petName}
+          </Menu.Item>
         ))
       ) : (
         <Menu.Item key="register">서비스가 처음이신가요?</Menu.Item>
+      )}
+      {petList.length > 0 && (
+        <div
+          style={{
+            position: "absolute",
+            bottom: "0",
+            width: "300px",
+            height: "12vh",
+            backgroundColor: "#4A85DC",
+          }}
+        >
+          <Profile data={selectedPet} />
+        </div>
       )}
     </Menu>
   );
